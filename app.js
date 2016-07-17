5ddb38b814ec74b0ead8b18cdec9dc5ff02d2ee5
derontron <derontron@gmail.com>
2016-07-17 18:46:54 -0400

    Create server, can display JSON results on app route from API http call

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+var ejs = require("ejs");
+
+
+
+app.get("/results", function(req, res){
+	
+	request("http://www.omdbapi.com/?s='Harry'", function(error, response, body){
+		if(!error && response.statusCode == 200) {
+			var parseData = JSON.parse(body);
+			res.send(parseData);
+		}
+	});
+
+});
+
+
+
+
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+// 	console.log("Server has started...");
+// });
+
+app.listen("5234", "localhost", function(){
+	console.log("Server has started...");
+});
